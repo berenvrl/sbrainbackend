fc4b7cd04b668f1e649917835784386168937fd2
Beren Varol <beren.vrl@gmail.com>
2024-01-19 15:37:12 +0300

    port changed

--- server.js
+++ server.js
@@ -72,8 +72,8 @@ app.post("/imageurl", (req, res) => {
 //   console.log(`Server is listening on port ${process.env.PORT}`);
 // });
 
-app.listen(3000, () => {
-  console.log(`Server is listening on port 3000`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`);
 });
 
 /*
